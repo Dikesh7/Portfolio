da71942c8ef5ca2eb86868778312b96fa09d3fe9
Dikesh7 <139131882+Dikesh7@users.noreply.github.com>
2023-09-06 12:30:08 +0545

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ document.querySelector('.hamburger').addEventListener("click", ()=>{
         },350);
     }
 })
+
+// TypeScript code for the text animation
+const textElement = document.querySelector('.animated-text');
+const text = textElement.textContent || '';
+const delay = 80;
+
+function animateText() {
+    let index = 0;
+    textElement.textContent = '';
+
+    function addNextCharacter() {
+        if (index < text.length) {
+            textElement.textContent += text.charAt(index);
+            index++;
+            setTimeout(addNextCharacter, delay);
+        }
+    }
+
+    addNextCharacter();
+}
+
+animateText();
